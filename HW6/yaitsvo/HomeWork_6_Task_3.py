269a41772a7f81c287e047247c75193a1add632c
vladimir.yaitskyi <yaitskyi.vladimir@gmail.com>
2022-05-18 22:29:55 +0300

    Added HomeWork

--- HW6/yaitsvo/HomeWork_6_Task_3.py
+++ HW6/yaitsvo/HomeWork_6_Task_3.py
@@ -1,17 +1,4 @@
 def char_counter(stroka):
-    list_of_chars = []
-    for char in stroka:
-        if char not in list_of_chars:
-            list_of_chars.append(char)
-    result = dict.fromkeys(list_of_chars, 0)
-    for char in stroka:
-        count = 0
-        for chars in stroka:
-            if char == chars:
-                count += 1
-        result[char] = count
-    print(result)
+    return {i: stroka.lower().count(i) for i in stroka.lower()}
+print(char_counter(input()))
 
-s = input().lower()
-
-char_counter(s)
