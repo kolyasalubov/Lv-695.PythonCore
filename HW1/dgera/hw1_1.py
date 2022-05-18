2c2738ba2bf9e0f215cfe61186751fcf13a71574
Dmytro Gerasymenko <dn190783gda@gmail.com>
2022-05-18 17:54:25 +0300

    HW1 folder is created. Includes homework files.

---
+++ HW1/dgera/hw1_1.py
@@ -0,0 +1,8 @@
+name = input('What is your name? ')
+age = input('How old are you? ')
+city = input('Where do you live? ')
+
+print()
+print('Hello,', name, end=',\n')
+print('Your age is', age, end=',\n')
+print('You live in', city, end='.\n')
---
+++ HW1/dgera/hw1_2.py
@@ -0,0 +1,8 @@
+num_a = int(input('Input value for A: '))
+num_b = int(input('Input value for B: '))
+
+print('A + B =', num_a+num_b)
+print('A - B =', num_a-num_b)
+print('A * B =', num_a*num_b)
+print('A / B =', num_a/num_b)
+print('A ** B =', num_a**num_b)
