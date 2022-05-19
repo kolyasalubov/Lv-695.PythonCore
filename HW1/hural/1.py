9c7817f16d5f53025eb61639b9592dafe26bbb19
Hural <huralmartha@gmail.com>
2022-05-19 13:27:29 +0300

    added my homework

---
+++ HW1/hural/1.py
@@ -0,0 +1,7 @@
+#Some practice presentation_1
+name = input("Enter your name:")
+print("Hello," + name)
+age=input("How old are you:")
+print ("Your age is: " + age)
+where= input("Where are do you live:")
+print("You live in: " + where)
---
+++ HW1/hural/2.py
@@ -0,0 +1,8 @@
+#Time to practice_2
+a=100
+b=5
+print("a+b=",a+b)
+print("a-b=",a-b)
+print("a*b=",a*b)
+print("a/b=",a/b)
+print("a**b=",a**b)
