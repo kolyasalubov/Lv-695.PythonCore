8769e0527754e1bdfc7722a5f79c2dd4b11eacb7
Hural <huralmartha@gmail.com>
2022-05-19 14:14:02 +0300

    added hm

---
+++ HW5/hural/5.1.py
@@ -0,0 +1,6 @@
+evennumbers= [2**i for i in range(10)]
+print(evennumbers)
+oddnumber=[i for i in range(10) if i%2==1]
+print(oddnumber)
+notdivisible=[i for i in range(20) if i%3!=0]
+print(notdivisible)
