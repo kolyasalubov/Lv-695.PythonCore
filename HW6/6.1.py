7bcdc11a0301617f526d60deb9b9b3e41cfef733
Hural <huralmartha@gmail.com>
2022-05-19 17:18:17 +0300

    added hw

--- HW6/6.1.py
+++ HW6/6.1.py
@@ -1,10 +1,9 @@
 #function that returns the largest number of two numbers 
-def largest_number(x,y):
+def maxnumber(x,y):
     if x >= y:
         return x ,
     else:
         return y  
-
-x=10
+x=2
 y=15
-print(largest_number(x,y))
+print(maxnumber(x, y))
