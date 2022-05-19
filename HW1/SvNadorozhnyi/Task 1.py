742a039c35d9327d944c2beb7f8b1fdb0ba34e49
Svyatoslav Nadorozhnyi <svyatoslavnadorozhniy@gmail.com>
2022-05-19 14:49:22 +0300

    added HW1

---
+++ HW1/SvNadorozhnyi/Task 1.py
@@ -0,0 +1,7 @@
+enter_name = input("What is your name? ")
+enter_age = input("How old are you? ")
+enter_city = input("Where do you live? ")
+
+print("Hello,", enter_name)
+print("Your age is", enter_age)
+print("You live in", enter_city)
---
+++ HW1/SvNadorozhnyi/Task 2.py
@@ -0,0 +1,8 @@
+first_number = int(input("Enter your first number: "))
+second_number = int(input("Enter your second number: "))
+
+print(f"{first_number} + {second_number} = {first_number + second_number}")
+print(f"{first_number} - {second_number} = {first_number - second_number}")
+print(f"{first_number} * {second_number} = {first_number * second_number}")
+print(f"{first_number} / {second_number} = {first_number / second_number}")
+print(f"{first_number} ** {second_number} = {first_number ** second_number}")
