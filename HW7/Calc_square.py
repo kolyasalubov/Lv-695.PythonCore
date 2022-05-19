933d8b403feb082530b92e78145f677958ce2387
Olena Yazykova <e.k.yazykova@gmail.com>
2022-05-19 23:42:55 +0300

    Added homework 7

---
+++ HW7/Calc_square.py
@@ -0,0 +1,16 @@
+import math
+
+def areaOfRectangle(length_of_rectangle: float, width_of_rectangle: float) ->float:
+    """The function returns the area of rectangle."""
+
+    return round(length_of_rectangle*width_of_rectangle,2)
+
+def areaOfTriangle(side_of_triangle: float, height_of_triangle: float) ->float:
+    """The function returns the area of triangle."""
+
+    return round(0.5*side_of_triangle*height_of_triangle,2)
+
+def areaOfCircle(radius_of_circle: float) ->float:
+    """The function returns the area of circle."""
+
+    return round(math.pi*math.pow(radius_of_circle,2),2)
---
+++ HW7/Calc_square_main.py
@@ -0,0 +1,22 @@
+import Calc_square
+
+key=True
+while key:
+    geom_figure=int(input("Select the geometric figure: rectangle enter 1, triangle enter 2, circle enter 3: "))
+    if geom_figure==1:
+        key=False
+        length_of_rectangle=float(input("Set the length of the rectangle (in cm): "))
+        width_of_rectangle=float(input("Set the width of the rectangle (in cm): "))
+        print(f"The area of the rectangle is {Calc_square.areaOfRectangle(length_of_rectangle,width_of_rectangle)} sq.cm.")
+    elif geom_figure==2:
+        key=False
+        side_of_triangle=float(input("Set the base side of the triangle (in cm): "))
+        height_of_triangle=float(input("Set the height of the triangle (in cm): "))
+        print(f"The area of the triangle is {Calc_square.areaOfTriangle(side_of_triangle,height_of_triangle)} sq.cm.")
+    elif geom_figure==3:
+        key=False
+        radius_of_circle=float(input("Set the radius of the circle (in cm): "))
+        print(f"The area of the circle is {Calc_square.areaOfCircle(radius_of_circle)} sq.cm.")
+    else:
+        key=True
+        print("Data format error!")
