c45c342d608e7f9791a1be5574cb054cd3574639
Yehor Hrechykhin <grechihin.yegor@gmail.com>
2022-05-21 16:30:44 +0300

    added HW5_2

--- HW5/Yhrechykhin/HW5_2.py
+++ HW5/Yhrechykhin/HW5_2.py
@@ -1,15 +1,6 @@
 login = input('Please enter your login: ')
 
-data_list = {}
-
-data_list['login'] = login
-
-while True:
-    if data_list['login'] == 'First':
-        print('Welcome user!!!')
-        break
-    else:
-        print('Error message: try again')
-        break
-
-    
+while login != 'First':
+    login = input('Error message: wrong login\nPlease enter your login: ')
+else:
+    print(f'Welcome, {login}!')
