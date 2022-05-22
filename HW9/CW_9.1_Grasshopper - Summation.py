796d01093850e5d5cf21eb9d8c83c56910315b60
Oleh Kupriienko <kupriyenko.oleh@gmail.com>
2022-05-22 10:49:55 +0300

    fixed HW9

--- HW9/CW_9.1_Grasshopper - Summation.py
+++
@@ -1,8 +0,0 @@
-n = int(input('Please, input the number: n = '))
-
-
-def summation(num):
-    return sum(range(1, num+1))
-
-
-print(summation(n))
--- HW9/CW_9.3_Double Char.py
+++
@@ -1,8 +0,0 @@
-test = "Hello World"
-
-
-def double_string(s):
-    return ''.join(i*2 for i in s)
-
-
-print(double_string(test))
