0a08847df98d77c5c0b0867640031defb37b6601
Volodymyr Bei <67003121+volodymyrbey1121@users.noreply.github.com>
2022-05-22 22:59:28 +0300

    Add files via upload

---
+++ hw1.py
@@ -0,0 +1,6 @@
+name = input('What is your name?\n')
+age = input('How old are you?\n')
+country = input('Where are you from?\n')
+print('Hello', name)
+print('Your age is', age)
+print('You live in', country)
