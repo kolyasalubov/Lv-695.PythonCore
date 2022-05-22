7cc64cb1b06fd3c6ad1fd63f27a59ff086aa0fc1
Oleh Kupriienko <kupriyenko.oleh@gmail.com>
2022-05-22 10:41:45 +0300

    added half of HW9

---
+++ HW9/CW_9.1_Grasshopper - Summation.py
@@ -0,0 +1,8 @@
+n = int(input('Please, input the number: n = '))
+
+
+def summation(num):
+    return sum(range(1, num+1))
+
+
+print(summation(n))
---
+++ HW9/CW_9.3_Double Char.py
@@ -0,0 +1,8 @@
+test = "Hello World"
+
+
+def double_string(s):
+    return ''.join(i*2 for i in s)
+
+
+print(double_string(test))
