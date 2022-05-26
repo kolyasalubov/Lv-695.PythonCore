8ddb11732a6737470051c4d16895055881259551
Grysuk <dimkatinker@gmail.com>
2022-05-26 09:44:41 +0300

    added homework1

---
+++ homework1/homework1.py
@@ -0,0 +1,7 @@
+name = input('What is your name? ')
+age = input('How old are you? ')
+live = input('Where do you live? ')
+
+print('Hello', name)
+print('Your age is', age)
+print('You live in', live)
