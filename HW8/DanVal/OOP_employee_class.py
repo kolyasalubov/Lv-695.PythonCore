365abd2651c909a938108f0e4e588b92c72d83e5
DanilovValeriy <nashmultik@gmail.com>
2022-05-27 10:41:16 +0300

    OOP class Employee finish

--- HW8/DanVal/OOP_employee_class.py
+++ HW8/DanVal/OOP_employee_class.py
@@ -1,4 +1,4 @@
-class Employee:
+class Employee(object):
 
     employee_calc = 0
 
@@ -12,6 +12,16 @@ class Employee:
     
     def information(self):
         return f"Employee name - {self.name}. He/She has salary - {self.salary}"
-
+    
     def __del__(self):
         print(f"I'm deleting you data, Mr.{self.name}.\nWith love, your GarbageCollector.\nHA-HA-HA-HUM")
+
+# function out of class
+def all_information(my_class):
+    return f"Parent's class is {(my_class.__base__)}.\nThe class namespace - {my_class.__dict__}\nClass name - {my_class.__name__}\nThe module name in which the class is defined {my_class.__module__}\nThe documentation bar - {my_class.__doc__}"
+
+    
+a = Employee('Ashot', 1000)
+
+print(all_information(Employee))
+# my_value = {'Parent class is': '__base__','The class namespace': '__dict__','Class name': '__name__','The module name in which the class is defined': '__module__','The documentation bar ': '__doc__'}
