e84943767ad0ebe544127263c57aa9e01d2de566
yevhen_shpyrka <shpyrkayevhen@gmail.com>
2022-05-29 20:40:21 +0200

    added HW1

---
+++ HW1/nesik/1.py
@@ -0,0 +1,7 @@
+name = input('What\'s your name? ').strip()
+age = input('How old are you? ').strip()
+coutry_city = input('Where do you live? ').strip()
+
+print(f'Hello, {name.capitalize()}')
+print(f'Your age is {age}')
+print(f'You live in {coutry_city.capitalize()}')
---
+++ HW1/nesik/2.py
@@ -0,0 +1,8 @@
+a = int(input('Enter first number: ').strip())
+b = int(input('Enter second number: ').strip())
+
+print(f'a + b = {a + b}')
+print(f'a - b = {a - b}')
+print(f'a * b = {a * b}')
+print(f'a / b = {round((a / b), 2)}')
+print(f'a ** b = {a ** b}')
