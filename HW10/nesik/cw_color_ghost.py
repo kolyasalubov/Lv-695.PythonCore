e3d7c96d3c31597f360c7ef792e67a2abc3da8b9
yevhen_shpyrka <shpyrkayevhen@gmail.com>
2022-06-01 23:16:54 +0200

    added HW10

--- HW10/nesik/cw_color_ghost.py
+++ HW10/nesik/cw_color_ghost.py
@@ -2,7 +2,7 @@ import random
 
 
 class Ghost():
-    colors = ["white", "yellow", "purple", "red"]
+    colors = ["white", "yellow", "purple", "gold"]
 
     def __init__(self):
         self.color = random.choice(Ghost.colors)
